6d75a899072fe5f79d900f9ed003a11418dec9d4
b1tank <b1tank@outlook.com>
2025-07-10 09:44:16 -0700

    init project

---
+++ Tayan/TayanApp.swift
@@ -0,0 +1,17 @@
+//
+//  TayanApp.swift
+//  Tayan
+//
+//  Created by Zhichao Li on 7/10/25.
+//
+
+import SwiftUI
+
+@main
+struct TayanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
